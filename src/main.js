2b990ea2dd6a76cb7e2e9da1c82640f57b7737e4
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-07 17:02:16 -0600

    Funcion multiplos de un rango convertidos a string

--- src/main.js
+++ src/main.js
@@ -1,27 +1,53 @@
 export default class App {
     sumatoriaSerieUno(numero) {
-        let suma = 1;
-        for (let i = 2; i <= numero; i++) {
-          suma = suma + 1 / i;
+    let suma = 1;
+    for (let i = 2; i <= numero; i++) {
+        suma = suma + 1 / i;
+    }
+    return suma;
+    }
+
+    sumatoriaSerieDos(numero) {
+    let suma = 1;
+    for (let i = 2; i <= numero; i++) {
+        if (i % 2 === 0) {
+        suma = suma + 1 / i;
+        } else {
+        suma = suma - 1 / i;
         }
-        return suma;
-      }
+    }
+    return suma;
+    }
     
-      sumatoriaSerieDos(numero) {
-        let suma = 1;
-        for (let i = 2; i <= numero; i++) {
-          if (i % 2 === 0) {
-            suma = suma + 1 / i;
-          } else {
-            suma = suma - 1 / i;
-          }
+    esPrimo(numero){
+    let i, flag;
+    if(numero == 0 | numero == 1 | numero == 4){
+        return false;
+    }
+
+    for(i=2; i<numero/2; i++){
+        if(numero % i == 0){
+            return false;
         }
-        return suma;
-      }
-    
-      esPrimo(numero){
-    
-      }
+    }
+    return true;
+    }
+    obtenerMultiplos(inicio, fin){
+        let t,suma,i;
+        if(inicio > fin){
+            t = inicio;
+            inicio = fin;
+            fin = t;
+        }
+        i = inicio;
+        do{
+        if(i % 3 == 0){
+            suma += i.toString();
+        }
+        i++;
+
+        }while(i <= fin);
+        }
 }
 
 let app = new App();
@@ -34,4 +60,10 @@ console.log("Funcion sumatoria de la serie dos: ");
 console.log(app.sumatoriaSerieDos(5));
 console.log(app.sumatoriaSerieDos(10));
 console.log(app.sumatoriaSerieDos(15));
+console.log("");
 console.log("Funcion si un numero es primo o no:");
+console.log(app.esPrimo(0));
+console.log(app.esPrimo(7));
+console.log("");
+console.log("Funcion multiplos de un rango convertidos a string: ");
+console.log(app.obtenerMultiplos(12,10));
