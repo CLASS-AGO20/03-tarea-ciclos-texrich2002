0113aed275afb83b9065137f0b2eb2d3c41e69d9
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-07 16:24:52 -0600

    Funcion sumatoria de la serie uno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,37 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero) {
+        let suma = 1;
+        for (let i = 2; i <= numero; i++) {
+          suma = suma + 1 / i;
+        }
+        return suma;
+      }
+    
+      sumatoriaSerieDos(numero) {
+        let suma = 1;
+        for (let i = 2; i <= numero; i++) {
+          if (i % 2 === 0) {
+            suma = suma + 1 / i;
+          } else {
+            suma = suma - 1 / i;
+          }
+        }
+        return suma;
+      }
+    
+      esPrimo(numero){
+    
+      }
+}
 
 let app = new App();
+console.log("Funcion sumatoria de la serie uno: ");
+console.log(app.sumatoriaSerieUno(7));
+console.log(app.sumatoriaSerieUno(15));
+console.log(app.sumatoriaSerieUno(21));
+console.log("");
+console.log("Funcion sumatoria de la serie dos: ");
+console.log(app.sumatoriaSerieDos(7));
+console.log(app.sumatoriaSerieDos(15));
+console.log(app.sumatoriaSerieDos(21));
+console.log("Funcion si un numero es primo o no:");
